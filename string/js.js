64ff23a0eeb4f2cbf9e7e429c056fe09fbc7eacf
JaloliddinKulmamatov <itjalol2010@gmail.com>
2023-12-25 00:00:17 +0500

    first commit

---
+++ string/js.js
@@ -0,0 +1,63 @@
+//str1
+
+// let n = 28;
+
+//     if (n >= 1 && n <= 26) {
+//         const alfavit = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+//         for (let i = 0; i < n; i++) {
+//             let harf = alfavit[i];
+//             console.log(harf);
+//         }
+//     } else {{
+//         console.log("Notog'ri son kirittingiz");
+//     }
+// }
+
+// console.log();
+
+//str2
+
+// let n = "кппарьпварт";
+
+// if (n / n == 1) {
+//     console.log("digint");
+// }
+// else  if(n.includes([...'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm""'])) {
+//     console.log("lotin");}
+// else {
+//     console.log("nol");
+// }
+
+//str3
+
+// let str = "eqwjmmgfds wwdh";
+// let first = str.slice(0, 1);
+// let end = str.slice(-1) ;
+
+// console.log(first.codePointAt());
+// console.log(end.codePointAt());
+
+//str4
+
+// let n = 9;
+// let b = "w";
+// var r = 0;
+// console.log( r = b.repeat(n));
+
+//str5
+
+// let str = `wfghgfd wert`;
+
+// console.log([...str].reverse().join(""));
+
+//str6
+
+let s = 235;
+let c = 0;
+
+while (s <= 0) {
+    s / 10;
+    c++;
+    
+}
